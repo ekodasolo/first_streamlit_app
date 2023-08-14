fe5d5190675f6f23cb3710238320b6e6c7645b58
ekodasolo <rddbank4@gmail.com>
2023-08-14 11:42:49 +0900

    Update streamlit_app.py
    Add breakfast menu and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit
 
-streamlit.title("My Parents new healthy diner")
+# Title
+streamlit.title('My Parents new healthy diner')
 
+# Body
+streamlit.header('Breakfast Menu')
+
+# Menu
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Pocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
