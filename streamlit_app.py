a5ef6c8103c26111df37f37872e616f38700f54f
ekodasolo <rddbank4@gmail.com>
2023-08-14 11:47:00 +0900

    Update streamlit_app.py
    Create another header for a special menu item.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Pocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+# Header
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
