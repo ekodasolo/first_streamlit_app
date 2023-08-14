86b8e6ea91ece3353d99ab96a357f2a06483a94e
ekodasolo <rddbank4@gmail.com>
2023-08-14 13:50:34 +0900

    Update streamlit_app.py
    Add: Filter table values

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+fruits_to_show  = my_fruit_list.loc[fruits_selected]
 
 # Data Table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
