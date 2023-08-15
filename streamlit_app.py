45a37dcbfdf755b2dfa875c1346ab29030804e1c
ekodasolo <rddbank4@gmail.com>
2023-08-15 11:02:18 +0900

    Update streamlit_app.py
    Add: dataframe and so on.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruites:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 fruits_to_show  = my_fruit_list.loc[fruits_selected]
+
+# Data Table
+streamlit.dataframe(fruits_to_show)
+
+# New section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
